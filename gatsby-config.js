73f155e0888b99414623ccd48cc4e07be535fb6e
matt.huntsberry <matt.huntsberry@nielsen.com>
2021-09-30 11:19:07 -0700

    Remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,10 +55,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "UA-125057966-1", // Google Analytics / GA
-          // optional
-        ],
+        trackingIds: ["UA-125057966-1"],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
