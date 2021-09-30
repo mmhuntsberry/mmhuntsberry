5d2dd807d183caa8f16b40914ae410f1454f94df
matt.huntsberry <matt.huntsberry@nielsen.com>
2021-09-30 11:58:07 -0700

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,23 +54,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ["UA-125057966-1"],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
+        trackingIds: "UA-125057966-1",
+        head: true,
+        anonymize: true,
       },
     },
     {
